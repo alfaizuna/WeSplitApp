986b6e2e57c6c0ca51f512c68f32a058304ce286
alfaizuna <alfaizunaulia@gmail.com>
2024-09-07 09:27:01 +0700

    first commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Alfaizuna Aulia Ulloh on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
